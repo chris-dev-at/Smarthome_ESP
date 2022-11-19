bc1fe195cf04071e2b375113ef0639bd32b6b287
Christian W <chris.dev.at@gmail.com>
2022-11-19 01:07:42 +0100

    Arduino TCP-Connection done. ORIGIN_COMMIT_OWNER: lukikoli, !ORIGIN COMMIT REMOVED AN REPLACED DUE TO SECURITY REASONS!

---
+++ Arduino/Server/Devices.ino
@@ -0,0 +1,20 @@
+class Device {
+  public: 
+    int Device_ID;
+    int State;
+
+    void On();
+    void Off();
+    
+  };
+
+class Relay : public Device{
+
+
+};
+
+
+class Transmitter_433 {
+
+
+};
--- Arduino/Server/Server.ino
+++ Arduino/Server/Server.ino
@@ -1,12 +1,77 @@
-#include <Wifi.h>
+#include <ESP8266WiFi.h>
+
+
+const char* WIFI_SSID = "WIFI_SSID";
+const char* WIFI_PASSWORD = "WIFI_PASSWORD";
+#define SERVER_PORT 25567
+
+
+WiFiServer server(SERVER_PORT);
+
+WiFiClient client;
+
+
 
 void setup() {
-  // put your setup code here, to run once:
-  
-
+  initHardware();
+  setupWiFi();
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+
+  TCPServer();
+}
+
+
+
+String Message = "";
+char lastchar = ' ';
+
+void TCPServer() {
+  while (lastchar != '.') {
+    if (!client.connected()) {
+      // try to connect to a new client
+      client = server.available();
+    } else {
+      //Serial.println("Client connected");
+      // read data from the connected client
+
+      if (client.available() > 0) {
+        //Serial.println(client.read());
+        lastchar = client.read();
+
+        if (lastchar != 0) {
+          //Serial.print(lastchar);
+          Message = Message + lastchar;
+        }
+      }
+    }
+  }
+  if (lastchar == '.') {
+    Serial.println(Message);
+    Message = "";
+    //return Message;
+    lastchar = 0;
+  }
+}
+
+void setupWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.setHostname("ESP32_Buzzer");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connecting to Wifi... Try:");
+    Serial.println(".");
+    delay(100);
+  }
+
+  Serial.print("ESP32 #2: TCP Server IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void initHardware() {
+  Serial.begin(115200);
 }
